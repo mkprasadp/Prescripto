4b422a973f07fd6d4c129d6086bc6f62d2b71d3b
mkprasadp <manikantaprasad98664@gmail.com>
2025-11-19 20:14:07 +0530

    Doctors

--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './src/Config/db.js';
-import connectCloud from './src/Config/Cloudinary.js';
-import adminRoutes from './src/Routes/adminRoutes.js';
 
 const app = express();
-//const PORT = 5000 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/admin',adminRoutes)
+await connectDB();
 
 app.get('/',(req,res)=>{
     res.send('Server is running');
 })
 
-/*
-app.listen(PORT,()=>{
+app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 })
-*/
 
 export default app;
